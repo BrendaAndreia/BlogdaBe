4115634a9f1be110c40ff693b3649699507f3378
Brenda Andreia <46536940+BrendaAndreia@users.noreply.github.com>
2023-04-21 16:49:14 -0300

    Update settings.py

--- pagina/settings.py
+++ pagina/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-66z!l6q1!-em&viyct)(&wqk7miy1&hty+y6zka0(fm*r3ji(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogdabe-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
